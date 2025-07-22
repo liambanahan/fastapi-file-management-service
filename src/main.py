5a0bd70a470149a315b4c812e3baf01ab3dc2fab
Liam Banahan <lbanahan24@gmail.com>
2025-07-22 16:17:43 -0400

    added CORS handling for backend acceess

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from api.routes import file
 from exceptions.handler import ExceptionHandler
+from fastapi.middleware.cors import CORSMiddleware
 
 def create_application() -> FastAPI:
     app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:3000"], #allow frontend to access the backend
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     app.include_router(file.router)
     ExceptionHandler(app)
     return app
